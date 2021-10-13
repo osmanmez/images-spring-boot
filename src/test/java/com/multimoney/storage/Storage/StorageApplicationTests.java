2c768b8609b9526fdc3fb5c7a35e2790f260b32d
Osman Mazariegos <omazariegosmz@gmail.com>
2021-10-13 11:47:57 -0600

    commit initial

---
+++ src/test/java/com/multimoney/storage/Storage/StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multimoney.storage.Storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
