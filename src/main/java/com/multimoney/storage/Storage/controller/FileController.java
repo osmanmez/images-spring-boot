b6ac6d24f6e79b4c4d450c1572dfaeb596a51aee
Osman Mazariegos <omazariegosmz@gmail.com>
2021-10-13 18:18:34 -0600

    update file controller ++ post upload

--- src/main/java/com/multimoney/storage/Storage/controller/FileController.java
+++ src/main/java/com/multimoney/storage/Storage/controller/FileController.java
@@ -19,10 +19,28 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.multimoney.storage.Storage.service.FileStorageService;
 import com.multimoney.storage.Storage.message.ResponseMessage;
 
+import com.multimoney.storage.Storage.message.ResponseFile;
+
 import com.multimoney.storage.Storage.repository.FileRepository;
 
 @Controller
 @CrossOrigin("http://localhost:8081")
 public class FileController {
-    
+    @Autowired
+    private FileStorageService storageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+      String message = "";
+      try {
+        storageService.store(file);
+  
+        message = "Uploaded the file successfully: " + file.getOriginalFilename();
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+      } catch (Exception e) {
+        message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+      }
+    }
+
 }
